b2999415e19e550d332011df96a2ef86db23aefd
Jkim1122 <jkimz1122@gmail.com>
2023-10-12 13:44:48 -0500

    started

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,37 @@
+import random
+
+randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
+
 class BoggleBoard:
   
   def __init__(self):
-  
+    pass
+    
+    # line_one = "____"
+    # line_two = "____"
+    # line_three = "____"
+    # line_four = "____"
 
-  def shake(self):
+  # def shake(self):
+  #   for ltr in self.line_one:
+  #     ltr = randomUpperLetter
+
+# print(randomUpperLetter)
+
+
+welcome_message = """
+----  WELCOME  ---- 
+1. Start New Game
+2. Quit
+
+"""
+quit = False
+while quit == False:
+    user_input = input(f"""{welcome_message}What would you like to do? input one of the numbers to execute command: """)
+    
+
+
+new_boggle_board = [["____"], ["____"], ["____"], ["____"]]
+# while True:
+  #  print(new_boggle_board)
+print(new_boggle_board)

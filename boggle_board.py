cfac78a5b9c590d99b3846258f6c8433aad42835
Cody Hardy <chardy0919@gmail.com>
2023-10-12 15:47:48 -0400

    Cody, fix the git

---
+++ boggle_board.py
@@ -0,0 +1,40 @@
+import random
+randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
+
+class BoggleBoard:
+  
+
+  def __init__(self):
+    self.board = "____\n____\n____\n____\n"
+ 
+  def __str__(self):
+    return self.board
+  
+  def shake(self):
+    new_board = ""
+    lines = 4
+    while lines > 0:
+      new_line = ""
+      while len(new_line) < 4:
+        new_line += chr(random.randint(ord('A'), ord('Z')))
+      lines -= 1
+      new_board += new_line+'\n'
+    self.board = new_board 
+
+boggle_board = BoggleBoard()
+print(boggle_board)
+boggle_board.shake()
+print(boggle_board)
+
+# welcome_message = """
+# ----  WELCOME  ---- 
+# 1. Start New Game
+# 2. Quit
+
+# """
+# quit = False
+# while quit == False:
+#     user_input = input(f"""{welcome_message}What would you like to do? input one of the numbers to execute command: """)
+    
+
+
